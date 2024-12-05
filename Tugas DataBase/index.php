6c46a3a897aa58701c25eaa2022161214500a711
FAIZGANSS23 <faizaazkawahyuramadhan@saintek.unipdu.ac.id>
2024-12-05 19:24:50 +0700

    Add files via upload

---
+++ Tugas DataBase/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Siswa informasi sekolah</h1>
+        <h2>SMK unipdu</h2>
+        <a href="data_siswa.php">Daftar Siswa</a><br>
+        <a href="tambah_siswa.php">pendaftaran</a>
+    </body>
+</html>
